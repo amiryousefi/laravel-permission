e3e84d0a38e48818c07e8dfba02cfb9071d67f9c
ali <aahelali@gmail.com>
2024-12-06 19:05:54 +0330

    Add 2 scope to Role model

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace Amir\Permission\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
@@ -9,6 +10,16 @@ class Role extends Model
     public $timestamps = false;
 
     protected $fillable = ['name'];
+
+    public function scopeName(Builder $query, string $name)
+    {
+        $query->where('name', $name);
+    }
+
+    public function scopeNames(Builder $query, string $names)
+    {
+        $query->whereIn('name', $names);
+    }
 
     public function permissions()
     {
